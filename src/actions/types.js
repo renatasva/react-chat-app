b16d5a3ed174f374684c881fc53d7ce86b21c4d7
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-22 00:47:04 +0000

    added panels

---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+//this file is responsible for what type of change we want to make to our GLOBAL STATE
+export const SET_USER = 'SET_USER';
--- src/index.js
+++ src/index.js
@@ -3,27 +3,70 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login.jsx';
 import Register from './components/Auth/Register.jsx';
+import Spinner from './Spinner';
 import * as serviceWorker from './serviceWorker';
 import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
-class Root extends React.Component { 
+//we will be creating a lot of components, many of which are going to need to access data
+//from user object we should be able to execute the function 'setUser', we pass user data
+//and put all of that data on global state that all our app components could use it.
+//to do this - we set STATE MANAGEMENT SYSTEM - REDUX
+
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./reducers";
+import { setUser } from './actions';
+
+//to create our global state we create a store variable and this will store the return value
+//from executing the createStore function and for that we need to pass an empty function for now
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      }
+    })
+
+  }
+
   render() {
-    return (
-    <Router>
+    return this.props.isLoading ? <Spinner /> : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </Switch>
-    </Router>
-   );
+    );
   }
 }
-ReactDOM.render(<Root />, document.getElementById('root'));
+
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(
+    mapStateFromProps,
+    { setUser }
+  )(Root)
+);
+
+ReactDOM.render(
+//to provide that GLOBAL STATE to all of our components:
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
