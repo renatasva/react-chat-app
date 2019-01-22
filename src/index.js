7561f66a14a85a5f4cc4932e3871f7dbcbe115ab
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-22 10:47:18 +0000

    fixed error

--- src/index.js
+++ src/index.js
@@ -3,27 +3,70 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login.jsx';
 import Register from './components/Auth/Register.jsx';
+import Spinner from "./Spinner";
 import * as serviceWorker from './serviceWorker';
 import firebase from './firebase';
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 
-class Root extends React.Component { 
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // console.log(user);
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
+  }
+
   render() {
-    return (
-    <Router>
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </Switch>
-    </Router>
-   );
+    );
   }
 }
-ReactDOM.render(<Root />, document.getElementById('root'));
+
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(
+    mapStateFromProps,
+    { setUser }
+  )(Root)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
