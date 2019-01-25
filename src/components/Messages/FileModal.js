bf6f679ab62559ae118f1a7b661dff33afe4a5eb
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-25 12:35:26 +0000

    connected upload/post image to firebase

--- src/components/Messages/FileModal.js
+++ src/components/Messages/FileModal.js
@@ -1,14 +1,50 @@
 import React from "react";
+import mime from 'mime-types';
 import { Modal, Icon, Button, Input } from 'semantic-ui-react';
 
 class FileModal extends React.Component {
+  state = {
+    file: null,
+    authorized: ['image/jpeg', 'image/png']
+  };
+
+  addFile = event => {
+    const file = event.target.files[0];
+    //check if there is a file
+    if (file) {
+      this.setState({ file });
+    }
+  };
+
+  sendFile = () => {
+    const { file } = this.state;
+    const { uploadFile, closeModal } = this.props;
+    //check if the file property and state isn't empty
+    if (file !== null) {
+    //check if file type is allowed
+      if (this.isAuthorized(file.name)) {
+        //send file
+        const metadata = { contentType: mime.lookup(file.name) }; 
+        uploadFile(file, metadata);
+        closeModal();
+        this.clearFile();
+      }
+    }
+  };
+
+  isAuthorized = filename => this.state.authorized.includes(mime.lookup(filename));
+
+  clearFile = () => this.setState({ file: null });
+
   render() {
     const { modal, closeModal } = this.props;
+
     return (
       <Modal basic open={modal} onClose={closeModal}>
         <Modal.Header>Select an Image File</Modal.Header>
         <Modal.Content>
           <Input
+            onChange={this.addFile}
             fluidlabel="File types: jpg, png"
             name="file"
             type="file"
@@ -16,6 +52,7 @@ class FileModal extends React.Component {
         </Modal.Content>
         <Modal.Actions>
           <Button
+            onClick={this.sendFile}
             color="green"
             inverted
           >
--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,10 +1,16 @@
 import React from "react";
+import uuidv4 from "uuid/v4";
 import firebase from '../../firebase';
 import { Segment, Button, Input } from 'semantic-ui-react';
 import FileModal from './FileModal';
 
 class MessageForm extends React.Component {
   state = {
+    //with storageRef we reach out to firebase in order to store image
+    storageRef: firebase.storage().ref(),
+    uploadTask: null,
+    uploadState: "",
+    percentUploaded: 0,
     message: '',
     channel: this.props.currentChannel,
     user: this.props.currentUser,
@@ -21,18 +27,23 @@ class MessageForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  createMessage = () => {
+  createMessage = (fileUrl = null) => {
     const message = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
       user: {
         id: this.state.user.uid,
         name: this.state.user.displayName,
         avatar: this.state.user.photoURL
-      },
-      content: this.state.message
+      }
+    };
+    //we check if its a message with text content or a file content
+    if (fileUrl !== null) {
+      message["image"] = fileUrl;
+    } else {
+      message["content"] = this.state.message;
     }
     return message;
-  }
+  };
 
   sendMessage = () => {
     const { messagesRef } = this.props;
@@ -45,22 +56,96 @@ class MessageForm extends React.Component {
         .push()
         .set(this.createMessage())
         .then(() => {
-          this.setState({ loading: false, message: '', errors: [] })
+          this.setState({ loading: false, message: '', errors: [] });
         })
       .catch(err => {
         console.error(err);
         this.setState({
           loading: false,
           errors: this.state.errors.concat(err)
-        })
-      })
+        });
+      });
     } else {
       this.setState({
         errors: this.state.errors.concat({ message: 'Add a message' })
-      })
-
+      });
     }
-  }
+  };
+
+  //to upload an image and post it as a message
+  uploadFile = (file, metadata) => {
+    const pathToUpload = this.state.channel.id;
+    //we get our messages ref from props
+    const ref = this.props.messagesRef;
+    //we user uuidv4 function to create a random string
+    const filePath = `chat/public/${uuidv4()}.jpg`;
+
+    this.setState(
+      {
+        uploadState: "uploading",
+        //reference to our storageRef and put a child on it, witch is a file path
+        uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
+      },
+      //a callback funcion:
+      () => {
+        //we listen for state changes
+        this.state.uploadTask.on(
+          "state_changed",
+          snap => {
+            const percentUploaded = Math.round(
+              (snap.bytesTransferred / snap.totalBytes) * 100
+            );
+            this.setState({ percentUploaded });
+          },
+          //error callback catching any errors
+          err => {
+            console.error(err);
+            this.setState({
+              //if any errors - upload state with error and task to null
+              errors: this.state.errors.concat(err),
+              uploadState: "error",
+              uploadTask: null
+            });
+          },
+          //another callback witch reference a snapshot property on a uploadTask..
+          () => {
+            this.state.uploadTask.snapshot.ref
+              .getDownloadURL()
+              //a promise allowing us to get the url of the file that has been uploaded..
+              .then(downloadUrl => {
+                this.sendFileMessage(downloadUrl, ref, pathToUpload);
+              })
+              //since its a promise we are catching for any errors
+              .catch(err => {
+                console.error(err);
+                this.setState({
+                  errors: this.state.errors.concat(err),
+                  uploadState: "error",
+                  uploadTask: null
+                });
+              });
+          }
+        );
+      }
+    );
+  };
+
+  sendFileMessage = (fileUrl, ref, pathToUpload) => {
+    ref
+      .child(pathToUpload)
+      .push()
+      .set(this.createMessage(fileUrl))
+      .then(() => {
+        //after sending message we update stae to done
+        this.setState({ uploadState: "done" });
+      })
+      .catch(err => {
+        console.error(err);
+        this.setState({
+          errors: this.state.errors.concat(err)
+        });
+      });
+  };
 
   render() {
     const { errors, message, loading, modal } = this.state;
@@ -98,11 +183,12 @@ class MessageForm extends React.Component {
           <FileModal
             modal={modal}
             closeModal={this.closeModal}
+            uploadFile={this.uploadFile}
           />
         </Button.Group>
       </Segment>
     );
   }
-}
+};
 
 export default MessageForm;
